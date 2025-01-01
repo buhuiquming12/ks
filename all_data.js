a3cedb7f888e5330aa12e2ac7778fb53764c1d47
buhuiquming12 <142579821+buhuiquming12@users.noreply.github.com>
2025-01-01 14:01:04 +0800

    Add files via upload

---
+++ all_data.js
@@ -0,0 +1,79 @@
+const menulist = {
+    menus: [
+        {
+            name: "首页",
+            url: "#",
+            show: true,
+            subMenus: []
+        },
+        {
+            name: "分类浏览",
+            url: "#",
+            show: true,
+            subMenus: [{name:"热血", url:"#"}, {name:"恋爱", url:"#"}, {name:"科幻", url:"#"}]
+        },
+        {
+            name: "收藏夹",
+            url: "#",
+            show: true,
+            subMenus: [{name:"已收藏动漫", url:"#"}, {name:"观看历史", url:"#"}]
+        },
+        {
+            name: "排行榜",
+            url: "#",
+            show: true,
+            subMenus: [{name:"周排行", url:"#"}, {name:"月排行", url:"#"}]
+        }
+    ]
+}
+
+const anime_info = [
+        {
+            imagePath: '1.jpg',
+            title: '进击的巨人',
+            director: '谏山创',
+            rating: '9.8',
+            episodes: 75,
+            favorite: false
+        },
+        {
+            imagePath: '2.jpg',
+            title: '鬼灭之刃',
+            director: '吾峠呼世晴',
+            rating: '9.5',
+            episodes: 26,
+            favorite: false
+        },
+        {
+            imagePath: '3.jpg',
+            title: '间谍过家家',
+            director: '远藤达哉',
+            rating: '9.3',
+            episodes: 25,
+            favorite: false
+        },
+        {
+            imagePath: '4.jpg',
+            title: '咒术回战',
+            director: '芥见下下',
+            rating: '9.6',
+            episodes: 24,
+            favorite: false
+        },
+        {
+            imagePath: '5.jpg',
+            title: '一拳超人',
+            director: 'ONE',
+            rating: '9.4',
+            episodes: 24,
+            favorite: false
+        },
+        {
+            imagePath: '6.jpg',
+            title: '死亡笔记',
+            director: '大场鸫',
+            rating: '9.7',
+            episodes: 37,
+            favorite: false
+        }
+]
--- menuComponent.js
+++ menuComponent.js
@@ -1,16 +1,14 @@
-
 const menuComponent = {
-    props: ["menus","modelValue"],
-    emits: ["update:modelValue"],
+    props: ["menus"],
     template: `
     <ul v-bind="$attrs">
-        <li v-for="menu in menus">
-            <font style="" @click="folder(menu)" v-if="menu.show">-</font>
-            <font style="" @click="folder(menu)" v-if="!menu.show">+</font>
-            <a href="#" @click.prevent="$emit('update:modelValue', menu.name)">{{menu.name}}</a>
+        <li v-for="menu in menus" :key="menu.name">
+            <font style="cursor: pointer;" @click="folder(menu)" v-if="menu.show">-</font>
+            <font style="cursor: pointer;" @click="folder(menu)" v-if="!menu.show">+</font>
+            <router-link :to="convertToPath(menu.name)">{{menu.name}}</router-link>
             <ul v-show="menu.show">
-                <li v-for="submenu in menu.subMenus">
-                    <a href="#" @click.prevent="$emit('update:modelValue', submenu.name)">{{submenu.name}}</a>
+                <li v-for="submenu in menu.subMenus" :key="submenu.name">
+                    <router-link :to="convertToPath(submenu.name)">{{submenu.name}}</router-link>
                 </li>
             </ul>
         </li>
@@ -19,6 +17,22 @@ const menuComponent = {
     methods: {
         folder(menu){
             menu.show = !menu.show;
+        },
+        convertToPath(name) {
+            const pathMap = {
+                '首页': '/',
+                '分类浏览': '/category',
+                '热血': '/category',
+                '恋爱': '/category',
+                '科幻': '/category',
+                '收藏夹': '/favorite',
+                '已收藏动漫': '/favorite',
+                '观看历史': '/category',
+                '排行榜': '/ranking',
+                '周排行': '/ranking',
+                '月排行': '/ranking'
+            };
+            return pathMap[name];
         }
     }
 }
